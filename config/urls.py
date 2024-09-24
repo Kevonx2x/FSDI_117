3d157d28183b967b7acd24bf215b2c2b27ee33af
Turon Boyd <ron.business101@gmail.com>
2024-09-24 14:02:41 +0800

    Updates

--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from pages.views import home_view, about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name='home'),  # Root URL now serves the home view
-    path('home/', home_view, name='home'),  
-    path('about/', about_view, name='about'),  
+    path('home', home_view, name='home'), 
+    path('', about_view, name='about'), 
+    path('content/', include('content.urls')),
 ]

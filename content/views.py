d3c03551085f84f2284261a2e0d2f22ae2103c5b
Turon Boyd <ron.business101@gmail.com>
2024-09-26 17:25:16 +0800

    New Assignment

--- content/views.py
+++ content/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def projects_list(request):
-    return render(request, 'content/projects_list.html')
+
+    projects = Project.objects.all()
+
+
+
+    return render(
+        request,
+        'content/projects_list.html',
+            {"projects" : projects }
+        )
 
 def experience_list(request):
     return render(request, "content/experience_list.html")
